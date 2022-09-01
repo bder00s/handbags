37eab4a9347ed0179dabc984229db55d5fe0fbbf
bder00s <bonne-deroos@hotmail.com>
2022-09-01 20:56:59 +0200

    opzet project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 
 function App() {
   return (
+      <>
       <h1>Handbags & Purses</h1>
+      <nav className="nav">
+        <button className="button" type="button">to the collection</button>
+        <button className="button" type="button">shop all bags</button>
+        <button className="button" type="button">pre-orders</button>
+      </nav>
+        </>
   );
 }
 
