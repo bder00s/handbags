296e647efee14a4343ce971237dab303d226c133
bder00s <bonne-deroos@hotmail.com>
2022-09-02 18:14:59 +0200

    Tiles gemaakt

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./App.css";
 import Button from "./components/Button";
 import Product from "./components/Product";
+import Tile from "./components/Tile";
 import bag1 from "./assets/bag_1.png";
 import bag2 from "./assets/bag_2.png";
 import bag3 from "./assets/bag_3.png";
 import bag4 from "./assets/bag_4.png";
+import brand from "./assets/brand.png";
+import women from "./assets/our_story.png";
 
 function App() {
   return (
     <>
       <h1>Handbags & Purses</h1>
-      <nav className="nav">
+      <nav>
         <Button
           isDisabled={false}
           clickHandler={() => console.log("to the collection")}
@@ -50,27 +53,54 @@ function App() {
           price={250}
         />
 
-          <Product
-              redLabel="New collection"
-              image={bag3}
-              titleImage="Third bag"
-              name="The Simple bag"
-              price={300}
-              />
+        <Product
+          redLabel="New collection"
+          image={bag3}
+          titleImage="Third bag"
+          name="The Simple bag"
+          price={300}
+        />
 
-          <Product
-              redLabel="New collection"
-              image={bag4}
-              titleImage="Fourth bag"
-              name="The Trendy bag"
-              price="150"
-              />
+        <Product
+          redLabel="New collection"
+          image={bag4}
+          titleImage="Fourth bag"
+          name="The Trendy bag"
+          price="150"
+        />
       </main>
+
+      <footer>
+        <Tile tileTitle="The Brand">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+            consequatur debitis dignissimos distinctio, dolor doloribus est
+            facilis impedit nesciunt nobis numquam porro possimus, quisquam rem
+            saepe tempore veritatis. Odio, suscipit!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores,
+            quibusdam.
+          </p>
+        </Tile>
+
+        <Tile tileImage={brand} tileAltText="logo picture" />
+
+        <Tile tileImage={women} tileAltText="happy women" />
+
+        <Tile tileTitle="Our story">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            atque consequatur dignissimos et eveniet exercitationem, ipsa ipsum
+            necessitatibus, nisi odio officiis quae quidem repudiandae,
+            similique temporibus! Aspernatur distinctio minus mollitia officia
+            praesentium voluptatem! Aperiam atque dolores inventore iure,
+            provident reiciendis?
+          </p>
+        </Tile>
+      </footer>
     </>
   );
 }
 
 export default App;
-
-
-
---
+++ src/components/Tile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Tile({children, tileImage, tileAltText, tileTitle,tileText}) {
+    return (
+        <section>
+            {tileImage && <img src={tileImage} alt={tileAltText}/>}
+            {tileTitle && <h2>{tileTitle}</h2>}
+            {tileText &&<p>{tileText}</p>}
+            {children}
+
+        </section>
+
+    )
+}
+
+export default Tile
